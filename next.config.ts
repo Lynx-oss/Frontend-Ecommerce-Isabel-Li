70115289f0bd0361ae93d063146739c1e3c8c375
Ivan <ivannrodriguez008@gmail.com>
2026-01-22 01:46:10 -0300

    chore: add config images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8080',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
